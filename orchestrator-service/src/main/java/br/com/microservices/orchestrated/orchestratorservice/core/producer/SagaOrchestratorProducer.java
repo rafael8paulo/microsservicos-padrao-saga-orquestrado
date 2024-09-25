0785c404f87aa8c9af8f8c044f4e53411ffd1b0d
Rafael Paulo <rafael8paulo@gmail.com>
2024-09-24 21:57:17 -0300

    Criando os producers nas aplicações

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/SagaOrchestratorProducer.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/producer/SagaOrchestratorProducer.java
@@ -1,9 +1,7 @@
 package br.com.microservices.orchestrated.orchestratorservice.core.producer;
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
