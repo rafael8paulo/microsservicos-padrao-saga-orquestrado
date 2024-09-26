07364c8f3099c07fbcf6d6557b53344ac8342d59
Rafael Paulo <rafael8paulo@gmail.com>
2024-09-26 20:45:22 -0300

    Mapeando as DTOs e Documents e Repositories do microsserviço

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -4,6 +4,8 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,8 +14,10 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "event")
 public class Event {
 
+    @Id
     private String id;
     private String transactionId;
     private String orderId;
---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/dto/EventFilters.java
@@ -0,0 +1,15 @@
+package br.com.microservices.orchestrated.orderservice.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventFilters {
+
+    private String orderId;
+    private String transactionId;
+
+}
