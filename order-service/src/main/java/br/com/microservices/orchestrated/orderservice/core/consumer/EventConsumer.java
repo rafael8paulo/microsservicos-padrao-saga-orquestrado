cbcfcb0786c12aaea28173ffc63f46d77f9179be
Rafael Paulo <rafael8paulo@gmail.com>
2024-09-26 21:14:03 -0300

    Criando a lógica de recebimento de notificação de finalização da Saga

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.consumer;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class EventConsumer {
 
     private final JsonUtil jsonUtil;
+    private final EventService service;
 
     @KafkaListener(
             groupId = "${spring.kafka.consumer.group-id}",
@@ -20,7 +22,7 @@ public class EventConsumer {
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Receiving ending notification event {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        service.notifyEnding(event);
     }
 
 }
