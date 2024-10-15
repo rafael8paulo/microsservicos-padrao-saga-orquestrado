b38a75862e69e7356f9d32d0eec33fe2f6a87a46
Rafael Paulo <rafael8paulo@gmail.com>
2024-10-15 17:41:50 -0300

    Criando lógica para realizar pagamento com SUCESSO

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/History.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/dto/History.java
@@ -2,11 +2,13 @@ package br.com.microservices.orchestrated.paymentservice.core.dto;
 
 import br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/service/PaymentService.java
@@ -2,6 +2,8 @@ package br.com.microservices.orchestrated.paymentservice.core.service;
 
 import br.com.microservices.orchestrated.paymentservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.paymentservice.core.dto.Event;
+import br.com.microservices.orchestrated.paymentservice.core.dto.History;
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 import br.com.microservices.orchestrated.paymentservice.core.producer.KafkaProducer;
 import br.com.microservices.orchestrated.paymentservice.core.repository.PaymentRespository;
@@ -10,6 +12,10 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
+import static br.com.microservices.orchestrated.paymentservice.core.enums.ESagaStatus.SUCCESS;
+
 @Slf4j
 @Service
 @AllArgsConstructor
@@ -17,6 +23,7 @@ public class PaymentService {
 
     private static final String CURRENT_SOURCE = "PAYMENT_SERVICE";
     private static final Double REDUCE_SUM_VALUE = 0.0;
+    private static final Double MIN_AMOUNT_VALUE = 0.1;
 
     private final JsonUtil jsonUtil;
     private final KafkaProducer producer;
@@ -26,6 +33,10 @@ public class PaymentService {
         try {
             checkCurrentPayment(event);
             createPendingPayment(event);
+            var payment = findByOrderIdAndTransactionId(event);
+            validateAmount(payment.getTotalAmount());
+            changePaymentToSuccess(payment);
+            handleSuccess(event);
         } catch (Exception e) {
             log.error("Error trying to make payment: ", e);
         }
@@ -66,6 +77,24 @@ public class PaymentService {
                 .reduce(REDUCE_SUM_VALUE.intValue(), Integer::sum);
     }
 
+    public Payment findByOrderIdAndTransactionId(Event event) {
+        return paymentRespository
+                .findByOrderIdAndTransactionId(event.getOrderId(), event.getTransactionId())
+                .orElseThrow(() -> new ValidationException("Payment not found by OrderId and TransactionId"));
+    }
+
+    private void validateAmount(double amount) {
+        if (amount < MIN_AMOUNT_VALUE) {
+            throw new ValidationException("Payment amount must be greater than ".concat(MIN_AMOUNT_VALUE.toString()));
+        }
+    }
+
+    private void changePaymentToSuccess(Payment payment) {
+        payment.setStatus(EPaymentStatus.SUCCESS);
+        save(payment);
+    }
+
+
     private void save(Payment payment) {
         paymentRespository.save(payment);
     }
@@ -81,6 +110,24 @@ public class PaymentService {
     private void setEventAmountItems(Event event, Payment payment) {
         event.getPayload().setTotalAmount(payment.getTotalAmount());
         event.getPayload().setTotalItems(payment.getTotalItems());
+    }
+
+    private void handleSuccess(Event event) {
+        event.setStatus(SUCCESS);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Payment realized successfully!");
+    }
+
+    private void addHistory(Event event, String message) {
+        var history = History
+                .builder()
+                .source(event.getSource())
+                .status(event.getStatus())
+                .message(message)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        event.addToHistory(history);
     }
 
 }
