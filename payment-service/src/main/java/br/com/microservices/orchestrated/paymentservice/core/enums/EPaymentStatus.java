3fd272217c305e7859bda3bc152602b854cad27f
Rafael Paulo <rafael8paulo@gmail.com>
2024-10-15 16:46:29 -0300

    Modelando as classes e entidades de banco de dados

---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/enums/EPaymentStatus.java
@@ -0,0 +1,9 @@
+package br.com.microservices.orchestrated.paymentservice.core.enums;
+
+public enum EPaymentStatus {
+
+    PENDING,
+    SUCCESS,
+    REFUND
+
+}
---
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/model/Payment.java
@@ -0,0 +1,58 @@
+package br.com.microservices.orchestrated.paymentservice.core.model;
+
+import br.com.microservices.orchestrated.paymentservice.core.enums.EPaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private int totalItems;
+
+    @Column(nullable = false)
+    private double totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EPaymentStatus status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+        status = EPaymentStatus.PENDING;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
