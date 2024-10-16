ccb8cc29a23f9c3a8cfd7ed04a195b077f8f4ffb
Rafael Paulo <rafael8paulo@gmail.com>
2024-10-16 18:30:54 -0300

    Criando o SEC (Saga Exectution Controller) e toda a lógica da saga

--- orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/config/kafka/KafkaConfig.java
@@ -1,11 +1,11 @@
 package br.com.microservices.orchestrated.orchestratorservice.config.kafka;
 
-import br.com.microservices.orchestrated.orchestratorservice.core.enums.ETopics;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +23,8 @@ import static br.com.microservices.orchestrated.orchestratorservice.core.enums.E
 @RequiredArgsConstructor
 public class KafkaConfig {
 
-    private final static Integer PARTITION_COUNT = 1;
-    private final static Integer REPLICA_COUNT = 1;
+    private static final Integer PARTITION_COUNT = 1;
+    private static final Integer REPLICA_COUNT = 1;
 
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
@@ -58,8 +58,8 @@ public class KafkaConfig {
     private Map<String, Object> producerProps() {
         var props = new HashMap<String, Object>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
     }
 
@@ -71,8 +71,8 @@ public class KafkaConfig {
     private NewTopic buildTopic(String name) {
         return TopicBuilder
                 .name(name)
-                .replicas(PARTITION_COUNT)
-                .partitions(REPLICA_COUNT)
+                .partitions(PARTITION_COUNT)
+                .replicas(REPLICA_COUNT)
                 .build();
     }
 
@@ -97,26 +97,6 @@ public class KafkaConfig {
     }
 
     @Bean
-    public NewTopic inventorySuccessTopic() {
-        return buildTopic(INVENTORY_SUCCESS.getTopic());
-    }
-
-    @Bean
-    public NewTopic inventoryFailTopic() {
-        return buildTopic(INVENTORY_FAIL.getTopic());
-    }
-
-    @Bean
-    public NewTopic paymentSuccessTopic() {
-        return buildTopic(PAYMENT_SUCCESS.getTopic());
-    }
-
-    @Bean
-    public NewTopic paymentFailTopic() {
-        return buildTopic(PAYMENT_FAIL.getTopic());
-    }
-
-    @Bean
     public NewTopic productValidationSuccessTopic() {
         return buildTopic(PRODUCT_VALIDATION_SUCCESS.getTopic());
     }
@@ -127,8 +107,22 @@ public class KafkaConfig {
     }
 
     @Bean
-    public NewTopic notifyEndingTopic() {
-        return buildTopic(NOTIFY_ENDING.getTopic());
+    public NewTopic paymentSuccessTopic() {
+        return buildTopic(PAYMENT_SUCCESS.getTopic());
     }
 
+    @Bean
+    public NewTopic paymentValidationFailTopic() {
+        return buildTopic(PAYMENT_FAIL.getTopic());
+    }
+
+    @Bean
+    public NewTopic inventoryValidationSuccessTopic() {
+        return buildTopic(INVENTORY_SUCCESS.getTopic());
+    }
+
+    @Bean
+    public NewTopic inventoryValidationFailTopic() {
+        return buildTopic(INVENTORY_FAIL.getTopic());
+    }
 }
