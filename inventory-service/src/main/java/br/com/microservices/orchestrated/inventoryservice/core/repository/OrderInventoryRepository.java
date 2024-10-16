5128daa207c9bb73bf8f1dbe1157f00a275ef0dc
Rafael Paulo <rafael8paulo@gmail.com>
2024-10-16 13:48:17 -0300

    Criando lógica para rollback no estoque anterior

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/repository/OrderInventoryRepository.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/repository/OrderInventoryRepository.java
@@ -4,11 +4,10 @@ import br.com.microservices.orchestrated.inventoryservice.core.model.OrderInvent
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderInventoryRepository extends JpaRepository<OrderInventory, Integer> {
 
     Boolean existsByOrderIdAndTransactionId(String orderId, String transactionId);
-    List<Optional<OrderInventory>> findByOrderIdAndTransactionId(String orderId, String transactionId);
+    List<OrderInventory> findByOrderIdAndTransactionId(String orderId, String transactionId);
 
 }
--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/service/InventoryService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-import static br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus.SUCCESS;
+import static br.com.microservices.orchestrated.inventoryservice.core.enums.ESagaStatus.*;
 
 @Slf4j
 @Service
@@ -39,6 +39,7 @@ public class InventoryService {
             handleSuccess(event);
         } catch (Exception e) {
             log.error("Error trying to update inventory: ", e);
+            handleFailCurrentNotExecuted(event, e.getMessage());
         }
         producer.sendEvent(jsonUtil.toJson(event));
     }
@@ -110,6 +111,41 @@ public class InventoryService {
                 .build();
 
         event.addToHistory(history);
+    }
+
+    private void handleFailCurrentNotExecuted(Event event, String message) {
+        event.setStatus(ROLLBACK_PENDING);
+        event.setSource(CURRENT_SOURCE);
+        addHistory(event, "Fail to update inventory: ".concat(message));
+    }
+
+    public void rollbackInventory(Event event) {
+
+        event.setStatus(FAIL);
+        event.setSource(CURRENT_SOURCE);
+
+        try {
+            returnInventoryToPreviousValues(event);
+            addHistory(event, "Rollback executed on inventory!");
+        } catch (Exception e) {
+            addHistory(event, "Rollback not executed for inventory!".concat(e.getMessage()));
+        }
+
+        producer.sendEvent(jsonUtil.toJson(event));
+    }
+
+    private void returnInventoryToPreviousValues(Event event) {
+        orderInventoryRepository.findByOrderIdAndTransactionId(event.getPayload().getId(), event.getTransactionId())
+            .forEach(orderInventory -> {
+                var inventory = orderInventory.getInventory();
+                inventory.setAvailable(orderInventory.getOldQuantity());
+                inventoryRepository.save(inventory);
+                log.info("Restored inventory for order {} from {] to {}",
+                        event.getPayload().getId(),
+                        orderInventory.getNewQuantity(),
+                        inventory.getAvailable()
+                );
+        });
     }
 
 }
